9b2a910bc3dca743fb4c06d4a28796ae8f646fa1
Karol_M <keszua@gmail.com>
2023-03-24 09:53:22 +0100

    Generate user
    nest g mo user
    nest g co user
    nest g s user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ormConfig } from './config/ormconfig';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ormConfig } from './config/ormconfig';
       isGlobal: true,
     }),
     TypeOrmModule.forRoot(ormConfig),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
